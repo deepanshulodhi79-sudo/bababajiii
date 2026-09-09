deeb5b25fd908b0236d24ab5b588c0fc2bd35f79
deepanshulodhi79-sudo <deepanshulodhi79@gmail.com>
2026-09-10 01:54:14 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Secure Mail Console',
+  description: 'Bulk Email Sender',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
