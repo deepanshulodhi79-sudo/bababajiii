f5aa3af2b782ff8e1417a2e956695ea3af9b6de2
deepanshulodhi79-sudo <deepanshulodhi79@gmail.com>
2026-09-10 02:46:08 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -69,7 +69,7 @@ export default function BulkEmailSender() {
 
       // 2-Second Delay
       if (i < list.length - 1) {
-        await new Promise((res) => setTimeout(res, 2000));
+        await new Promise((res) => setTimeout(res, 800));
       }
     }
 
